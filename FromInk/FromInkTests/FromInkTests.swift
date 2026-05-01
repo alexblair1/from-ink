2d1999b5b77c47467d0274cae76ce4476251abc0
Alex Blair <alexblair190@gmail.com>
2026-05-01 15:03:37 -0500

    initial project commit

---
+++ FromInk/FromInkTests/FromInkTests.swift
@@ -0,0 +1,16 @@
+//
+//  FromInkTests.swift
+//  FromInkTests
+//
+//  Created by Alex Blair on 5/1/26.
+//
+
+import Testing
+
+struct FromInkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
